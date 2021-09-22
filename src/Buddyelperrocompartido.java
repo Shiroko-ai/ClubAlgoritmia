b5629ecbf691b65d4a9e6770fcf1a37fe4a6c887
Shiro <ivanpalacios2809@gmail.com>
2021-09-21 21:50:26 -0500

    Se agrega el problema de buddy

---
+++ src/Buddyelperrocompartido.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Buddyelperrocompartido {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Integer> porciones = new ArrayList<Integer>();
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                porciones.add(i);
+            }
+        }
+        for (int i = 0; i < porciones.size(); i++) {
+            if (i == 0) {
+                System.out.print(porciones.get(i));
+            } else {
+                System.out.print(" " + porciones.get(i));
+            }
+        }
+    }
+}
